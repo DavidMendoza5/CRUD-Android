7d97d2f72769d976959499a28d44bad45f553b85
David <cruz-azul99@hotmail.com>
2021-11-04 21:38:18 -0600

    User class created

--- app/src/main/java/com/example/androidwithsqllite/Users.java
+++ app/src/main/java/com/example/androidwithsqllite/Users.java
@@ -1,4 +1,32 @@
 package com.example.androidwithsqllite;
 
-public class Users {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+
+public class Users extends AppCompatActivity {
+    TextView name;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.show_users);
+
+        DataBase db = new DataBase(getApplicationContext());
+
+        List<UserModel> users = db.showUsers();
+
+        name = findViewById(R.id.users);
+
+        for (int i = 0; i < users.size(); i++) {
+           name.append("RFC:\t"+ users.get(i).getRfc() + "\nUser:\t" + users.get(i).getName() + "\n\n");
+        }
+
+    }
+
 }
