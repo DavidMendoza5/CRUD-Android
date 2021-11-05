bb42b13a779ffc61865c1527ec40f66a5fcbdff3
David <cruz-azul99@hotmail.com>
2021-11-04 21:37:47 -0600

    Code refactored

--- app/src/main/java/com/example/androidwithsqllite/MainActivity.java
+++ app/src/main/java/com/example/androidwithsqllite/MainActivity.java
@@ -2,37 +2,51 @@ package com.example.androidwithsqllite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText rfc, name, phone, email;
-    Button addUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void getUsers(View view) {
+        Intent i = new Intent(this, Users.class);
+        startActivity(i);
+    }
+
+    public void addNewUser(View view) {
         DataBase db = new DataBase(getApplicationContext());
+        UserModel userModel;
 
-        rfc = findViewById(R.id.rfcInput);
-        name = findViewById(R.id.nameInput);
-        phone = findViewById(R.id.phoneInput);
-        email = findViewById(R.id.emailInput);
+        userModel = getInputData();
+        db.addUser(userModel.getRfc(), userModel.getName(), userModel.getPhone(), userModel.getEmail());
+        Toast.makeText(getApplicationContext(), "Usuario agregado correctamente", Toast.LENGTH_SHORT).show();
 
-        addUser = findViewById(R.id.btnAdd);
+    }
 
-        addUser.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                db.addUser(rfc.getText().toString(), name.getText().toString(), Integer.parseInt(phone.getText().toString()),
-                        email.getText().toString());
-                Toast.makeText(getApplicationContext(), "Usuario agregado correctamente", Toast.LENGTH_SHORT).show();
-            }
-        });
+    public UserModel getInputData() {
+        EditText inputRfc, inputName, inputPhone, inputEmail;
+        String rfc, name, email;
+        int phone;
+
+        inputRfc = findViewById(R.id.rfcInput);
+        inputName = findViewById(R.id.nameInput);
+        inputPhone = findViewById(R.id.phoneInput);
+        inputEmail = findViewById(R.id.emailInput);
+
+        rfc = inputRfc.getText().toString();
+        name = inputName.getText().toString();
+        phone = Integer.parseInt(inputPhone.getText().toString());
+        email = inputEmail.getText().toString();
+
+        return new UserModel(rfc, name, phone, email);
     }
 }
