6f614613bdd226fe6c4e362e243237c680d578a4
David <cruz-azul99@hotmail.com>
2021-11-04 21:38:43 -0600

    User model created

--- app/src/main/java/com/example/androidwithsqllite/UserModel.java
+++ app/src/main/java/com/example/androidwithsqllite/UserModel.java
@@ -1,2 +1,33 @@
-package com.example.androidwithsqllite;public class UserModel {
+package com.example.androidwithsqllite;
+
+public class UserModel {
+    private String rfc, name, email;
+    private int phone;
+
+    public UserModel(){
+
+    }
+
+    public UserModel(String rfc, String name, int phone, String email) {
+        this.rfc = rfc;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
