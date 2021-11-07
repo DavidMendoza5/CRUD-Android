f027828f6a00ba438bc570e52ae44370cdbce2a2
David <cruz-azul99@hotmail.com>
2021-11-07 12:54:55 -0600

    onResume method added and searchUserByRfc method created

--- app/src/main/java/com/example/androidwithsqllite/Users.java
+++ app/src/main/java/com/example/androidwithsqllite/Users.java
@@ -1,6 +1,9 @@
 package com.example.androidwithsqllite;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -27,6 +30,32 @@ public class Users extends AppCompatActivity {
            name.append("RFC:\t"+ users.get(i).getRfc() + "\nUser:\t" + users.get(i).getName() + "\n\n");
         }
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        DataBase db = new DataBase(getApplicationContext());
+
+        List<UserModel> users = db.showUsers();
+
+        name = findViewById(R.id.users);
+
+        for (int i = 0; i < users.size(); i++) {
+            if(i == 0) {
+                name.setText("RFC:\t"+ users.get(i).getRfc() + "\nUser:\t" + users.get(i).getName() + "\n\n");
+            } else {
+                name.append("RFC:\t"+ users.get(i).getRfc() + "\nUser:\t" + users.get(i).getName() + "\n\n");
+            }
+        }
+    }
+
+    public  void searchUserByRfc(View view) {
+        EditText rfc = (EditText) findViewById(R.id.searchRFC);
+        Intent i = new Intent(this, User.class);
+        i.putExtra("userRfc", rfc.getText().toString());
+        startActivity(i);
     }
 
 }
